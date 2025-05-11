47d734f57e1fe64438cd5da3258d1c0a11186526
MaxxTheLightning <maksim.kryukov2009@gmail.com>
2025-05-11 12:32:25 +0300

    Working with shooting

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from pygame import *
-from random import randint
 
-#   GameSprite - main class for all sprites
+bullets = []
+
+#   Classes
 
 class GameSprite(sprite.Sprite):
 
-    def __init__(self, sprite_image, x, y, speed, scale):
+    def __init__(self, sprite_image, x, y, scale):
 
         super().__init__()
 
         self.image = transform.scale(image.load(sprite_image), (scale, scale))
-        self.speed = speed
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -19,11 +19,115 @@ class GameSprite(sprite.Sprite):
 
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+
+    def __init__(self, sprite_image, x, y, speed, scale):
+
+        super().__init__(sprite_image, x, y, scale)
+
+        self.shoot_counter = 0
+        self.speed = speed
+        self.health = 3
+
+    def update(self):
+
+        if keys_pressed[K_LEFT] and player.rect.x > 5:
+            
+            player.rect.x -= player.speed
+
+        if keys_pressed[K_RIGHT] and player.rect.x < 595:
+
+            player.rect.x += player.speed
+
+        if keys_pressed[K_UP] and player.rect.y > 5:
+
+            player.rect.y -= player.speed
+
+        if keys_pressed[K_DOWN] and player.rect.y < 395:
+
+            player.rect.y += player.speed
+
+        if keys_pressed[K_SPACE]:
+            
+            if self.shoot_counter == 0:
+
+                self.fire("right")
+
+            self.shoot_counter += 1
+
+            if self.shoot_counter > 20:
+
+                self.shoot_counter = 0
+        
+        else:
+
+            self.shoot_counter = 0
+
+    def fire(self, direction):
+
+        if direction == "right":
+
+            dir = 10
+            x = self.rect.x + 30
+
+        elif direction == "left":
+
+            dir = -10
+            x = self.rect.x - 30
+
+        bullet = Bullet('Media/Bullet.png', x, self.rect.y + 10, dir, 40)
+
+        if direction == "left":
+
+            bullet.image = transform.flip(bullet.image, True, False)
+
+        bullets.append(bullet)
+
+        fire.play()
+
+    def damage(self):
+
+        self.health -= 1
+
+class Bullet(GameSprite):
+
+    def __init__(self, sprite_image, x, y, speed, scale):
+
+        super().__init__(sprite_image, x, y, scale)
+
+        self.speed = speed
+
+    def update(self):
+
+        self.rect.x += self.speed
+
+#   Setup the scene
+
 window = display.set_mode((700, 500))
 display.set_caption("Space Shooter Beta")
 bg = image.load("Media/bg.jpg")
 background = transform.scale(bg, (700, 500))
-player = GameSprite('Media/player.png', 100, 100, 2, 60)
+
+#   Audioeffects setup
+
+mixer.init()
+fire = mixer.Sound('Media/new_fire.wav')
+
+#   Setup sprites
+
+player = Player('Media/player.png', 100, 100, 5, 60)
+
+hearts = []
+
+heart_x = 0
+
+for i in range(player.health):
+
+    heart = GameSprite('Media/Heart.png', heart_x, 5, 50)
+
+    hearts.append(heart)
+
+    heart_x += 50
 
 #   Game
 
@@ -39,6 +143,25 @@ while game:
     FPS = 60
     clock.tick(FPS)
 
+    #   Managing bullets
+
+    if len(bullets) > 0:
+
+        for this_bullet in bullets:
+
+            this_bullet.reset()
+            this_bullet.update()
+
+            if this_bullet.rect.x < -50 or this_bullet.rect.x > 750:
+
+                del bullets[bullets.index(this_bullet)]
+
+    if len(hearts) > 0:
+
+        for h in hearts:
+
+            h.reset()
+
     #   Application closing
 
     for e in event.get():
@@ -49,16 +172,6 @@ while game:
         
     keys_pressed = key.get_pressed()
 
-    if keys_pressed[K_LEFT] and player.rect.x > 5:
-       player.rect.x -= player.speed
-    if keys_pressed[K_RIGHT] and player.rect.x < 595:
-        player.rect.x += player.speed
-    if keys_pressed[K_UP] and player.rect.y > 5:
-        player.rect.y -= player.speed
-    if keys_pressed[K_DOWN] and player.rect.y < 395:
-        player.rect.y += player.speed
-
-
-
+    player.update()
 
     display.update()
